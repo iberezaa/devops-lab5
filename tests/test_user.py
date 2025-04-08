d34385afc47069fcec1afd7e058416cbb228f439
iberezaa <iberezdovec@gmail.com>
2025-04-08 21:17:39 +0300

    8

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,7 +3,6 @@ from src.main import app
 
 client = TestClient(app)
 
-# Существующие пользователи
 users = [
     {
         'id': 1,
@@ -34,7 +33,7 @@ def test_create_user_with_valid_email():
     new_user = {"name": "New User", "email": "newuser@mail.com"}
     response = client.post("/api/v1/user", json=new_user)
     assert response.status_code == 201
-    assert isinstance(response.json(), int)  # возвращается id пользователя
+    assert isinstance(response.json(), int)
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
@@ -46,11 +45,8 @@ def test_create_user_with_invalid_email():
 def test_delete_user():
     '''Удаление пользователя'''
     email = "deleteuser@mail.com"
-    # Сначала создаём пользователя
     client.post("/api/v1/user", json={"name": "To Delete", "email": email})
-    # Затем удаляем
     response = client.delete("/api/v1/user", params={"email": email})
     assert response.status_code == 204
-    # И проверим, что его больше нет
     response = client.get("/api/v1/user", params={"email": email})
     assert response.status_code == 404
